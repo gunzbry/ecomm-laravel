3c17631b803cade05e701659f602be968b1d651f
Bryan Loh <bryanloh@Bryans-MacBook-Pro.local>
2021-05-03 12:09:05 +0800

    #7 Homepage sider with all products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     function index(){
-        return "Welcome to product page";
+        // return Product::all();
+        //return "Welcome to product page";
+        $data = Product::all();
+        return view('product', ['products' => $data]);
     }
 }
