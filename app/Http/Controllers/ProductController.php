1bdf6d1ddfdddd2b8a6eeb8f90a9326cc4d4d9d0
Bryan Loh <bryanloh@Bryans-MacBook-Pro.local>
2021-05-03 21:16:35 +0800

    #14, Cart, header adjusts, Logout function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use Session;
 
 class ProductController extends Controller
 {
@@ -30,5 +32,22 @@ class ProductController extends Controller
                         'products' => $data
                     ]);
 
+    }
+
+    function add_to_cart(Request $req){
+        if($req->session()->has('user')){
+            $cart = new Cart();
+            $cart->user_id = $req->session()->get('user')['id'];
+            $cart->product_id = $req->product_id;
+            $cart->save();
+            return redirect('/');
+        }else{
+            return redirect('login');
+        }
+    }
+
+    static function cartItem(){
+        $userId = Session::get('user')['id'];
+        return Cart::where('user_id', $userId)->count();
     }
 }
