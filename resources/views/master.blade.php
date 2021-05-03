9c00a186b04cf5b5f3467253e4dca5bf4eea9d98
Bryan Loh <bryanloh@Bryans-MacBook-Pro.local>
2021-05-03 12:20:45 +0800

    #8 trending products, styling

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -34,7 +34,18 @@
         height : 600px;
     }
     .slider-text{
-        background-color: #35443585 !important;
+        background-
+        .color: #35443585 !important;
+    }
+    .trending-image{
+        height: 100px;
+    }
+    .trending-item {
+        float: left;
+        width: 20%;
+    }
+    .trending-wrapper{
+        margin: 30px;
     }
 </style>
 </html>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -20,16 +20,29 @@
                     </div>
                 </div>
             @endforeach
-      
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-            <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-            <span class="sr-only">Next</span>
-            </a>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <div class="trending-wrapper">
+        <h3>Trending Products</h3>
+        <div class="">
+            @foreach ($products as $item)
+                <div class="trending-item">
+                    <img class="trending-image" src="{{ $item['gallery'] }}">
+                    <div class="">
+                        <h3>{{ $item['name'] }}</h3>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
